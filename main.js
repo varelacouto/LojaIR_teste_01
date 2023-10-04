04f45ca960001ebd7436a75d1c465d7aa1f529c3
varelacouto <varela329@gmail.com>
2023-10-04 15:42:30 -0300

    upload site lojair01

---
+++ main.js
@@ -0,0 +1,11 @@
+﻿import { renderizarCatalogo } from "./src/cartaoProduto";
+import { inicializarCarrinho } from "./src/menuCarrinho";
+
+
+
+renderizarCatalogo();
+inicializarCarrinho();
+
+
+
+
---
+++ src/menuCarrinho.js
@@ -0,0 +1,68 @@
+﻿import { catalogo } from "./utilidades";
+
+const idsProdutoCarrinhoComQuantidade = {};
+
+function abrirCarrinho() {
+    document.getElementById("carrinho").classList.add("right-[0px]");
+    document.getElementById("carrinho").classList.remove("right-[-360px]");
+}
+
+function fecharCarrinho() {
+    document.getElementById("carrinho").classList.remove("right-[0px]");
+    document.getElementById("carrinho").classList.add("right-[-360px]");
+}
+
+export function inicializarCarrinho() {
+    const botaoFecharCarrinho = document.getElementById("fechar-carrinho");
+    const botaoAbrirCarrinho = document.getElementById("abrir-carrinho");
+
+    botaoFecharCarrinho.addEventListener("click", fecharCarrinho);
+    botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
+}
+
+function incrementarQuantidadeProduto(idProduto) {
+    idsProdutoCarrinhoComQuantidade[idProduto]++;
+    atulizarInformacaoQuantidade(idProduto);
+}
+
+function decrementarQuantidadeProduto(idProduto) {
+    idsProdutoCarrinhoComQuantidade[idProduto]--;
+    atulizarInformacaoQuantidade(idProduto);
+}
+
+function atulizarInformacaoQuantidade(idProduto) {
+    document.getElementById(`quantidade-${idProduto}`).innerText = idsProdutoCarrinhoComQuantidade[idProduto];
+}
+
+export function adicionarAoCarrinho(idProduto) {
+    if (idProduto in idsProdutoCarrinhoComQuantidade) {
+        incrementarQuantidadeProduto(idProduto);
+        return;
+    }
+    idsProdutoCarrinhoComQuantidade[idProduto] = 1;
+    const produto = catalogo.find((p) => p.id === idProduto);
+    const containerProdutosCarrinho = document.getElementById("produtos-carrinho");
+    const cartaoProdutoCarrinho = `
+    <article class="flex bg-sky-100 rounded-lg p-1 relative">
+        <button id="fechar-carrinho" class=" absolute top-0 right-3"><i class="fa-brands fa-xbox text-xs hover:text-red-500"></i></button>
+        <img src="./assets/img/${produto.imagem}" alt="Carrinho: ${produto.nome}" class="h-24 rounded-lg">
+        <div class="p-2 flex flex-col justify-between">
+          <p class="text-sm">${produto.nome}</p>
+          <p class="text-xs">Tamanho: 50cm x 50cm</p>
+          <p class="text-lg">R$${produto.preco}</p>
+        </div>
+        <div class="flex items-end absolute bottom-0 right-3 text-lg">
+            <button id="decrementar-produto-${produto.id}">-</button>
+            <p id="quantidade-${produto.id}" class="ml-2">${idsProdutoCarrinhoComQuantidade[produto.id]}</p>
+            <button class="ml-2" id="incrementar-produto-${produto.id}">+</button>
+        </div>
+
+    </article>
+    `;
+
+    containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
+
+    document.getElementById(`decrementar-produto-${produto.id}`).addEventListener("click", () => decrementarQuantidadeProduto(produto.id));
+
+    document.getElementById(`incrementar-produto-${produto.id}`).addEventListener("click", () => incrementarQuantidadeProduto(produto.id));
+}
